0b2417d4361f5c809317ae07906359a751b9201d
Ilham Wisnu <ilhamwisnu42@gmail.com>
2025-09-18 14:14:59 +0700

    init project

---
+++ StrollTaskOne/StrollTaskOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollTaskOneApp.swift
+//  StrollTaskOne
+//
+//  Created by Ilham Wisnu on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct StrollTaskOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
